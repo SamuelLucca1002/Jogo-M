791dd5f91b4dd910fbe15f2cd813ef9294d69e85
SamuelLucca1002 <ls.webdesigner0201@gmail.com>
2022-10-10 11:49:32 -0300

    jogo

--- controle.js
+++ controle.js
@@ -1,30 +1,30 @@
-const mario = document.querySelector(".mario");
-const pipe = document.querySelector(".pipe")
+const heroi = document.querySelector(".heroi");
+const pedra = document.querySelector(".pedra")
 
 const jump = () => {
-    mario.classList.add("jump");
+    heroi.classList.add("jump");
 
     setTimeout(() => {
-        mario.classList.remove("jump");
+        heroi.classList.remove("jump");
     }, 500)
 }
 
 const loop = setInterval(() => {
     
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
+    const pedraPosition = pedra.offsetLeft;
+    const heroiPosition = +window.getComputedStyle(heroi).bottom.replace("px", "");
 
-    if (pipePosition <= 120 && pipePosition >0 && marioPosition <80) {
+    if (pedraPosition <= 120 && pedraPosition >0 && heroiPosition <80) {
 
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`;
+        pedra.style.animation = 'none';
+        pedra.style.left = `${pedraPosition}px`;
 
-        mario.style.animation = 'none';
-        mario.style.bottom = `${marioPosition}px`;
+        heroi.style.animation = 'none';
+        heroi.style.bottom = `${heroiPosition}px`;
 
-        mario.src = "imagens/game-over.png"
-        mario.style.width = "80px"
-        mario.style.marginLeft = "50px"
+        heroi.src = "imagens/morte.png"
+        heroi.style.width = "500px"
+        heroi.style.marginLeft = "400px"
 
         clearInterval(loop);
     }
